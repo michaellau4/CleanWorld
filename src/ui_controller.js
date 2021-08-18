b39189fdf546303cda4cd8b42d1ea66507c07cd9
Michael Lau <lau.michael2@gmail.com>
2021-08-17 23:18:29 -0700

    added a ui controller

---
+++ src/ui_controller.js
@@ -0,0 +1,11 @@
+import { entity } from "./entity";
+
+export const ui_controller = (() => {
+  class UIController extends entity.Component {
+    constructor(params) {
+      super();
+      this._params = params;
+      this._items = [];
+    }
+  }
+})
